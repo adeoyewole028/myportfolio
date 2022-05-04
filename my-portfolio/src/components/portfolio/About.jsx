342ab438900c01d9119c91021d49d568a9b94f90
adeoyewole028 <adeoyewole028@gmail.com>
2022-05-04 19:56:30 +0100

    Fixed grammer

--- my-portfolio/src/components/portfolio/About.jsx
+++ my-portfolio/src/components/portfolio/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             <p>
               I am a frontend developer with a particular interest in making
               things simple and automating daily tasks. I try to keep up with
-              security and best practices, and am always looking for new things
+              security and best practices, and I am always looking for new things
               to learn.
             </p>
         </div>
